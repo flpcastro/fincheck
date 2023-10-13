470e6a04b24304b7ed82b49ca52a062c65f497c2
Felipe <flpc.dev@gmail.com>
2023-10-13 14:38:50 -0300

    feat: add ispublic decorator in auth public endpoints

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin';
 import { SignUpDto } from './dto/signup';
+import { IsPublic } from 'src/shared/decorators/IsPublic';
 
+@IsPublic()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -13,7 +15,7 @@ export class AuthController {
   }
 
   @Post('signup')
-  create(@Body() signUpDto: SignUpDto) {
+  signup(@Body() signUpDto: SignUpDto) {
     return this.authService.signup(signUpDto);
   }
 }
