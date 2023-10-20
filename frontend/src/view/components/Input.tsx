53b9ce25b19b3749b4ee9471d4417e4e7f70c431
Felipe <flpc.dev@gmail.com>
2023-10-19 23:26:25 -0300

    refactor: implement forwardref and add error props

--- frontend/src/view/components/Input.tsx
+++ frontend/src/view/components/Input.tsx
@@ -1,28 +1,46 @@
-import { ComponentProps } from "react";
+import { CrossCircledIcon } from "@radix-ui/react-icons";
+import { ComponentProps, forwardRef } from "react";
+import { cn } from "../../app/utils/cn";
 
 interface InputProps extends ComponentProps<'input'> {
   name: string;
+  error?: string;
 }
 
-export function Input({ name, id, placeholder, ...props}: InputProps) {
-  const inputId = id ?? name;
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ name, id, placeholder, error, ...props}, ref) => {
+    const inputId = id ?? name;
 
-  return (
-    <div className="relative">
-      <input
-        name={name}
-        id={inputId}
-        {...props}
-        className="w-full bg-white rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800 pt-4 peer placeholder-shown:pt-0 focus:border-gray-800 transition-all outline-none"
-        placeholder=" "
-      />
+    return (
+      <div className="relative">
+        <input
+          ref={ref}
+          name={name}
+          id={inputId}
+          placeholder=" "
+          {...props}
+          className={
+            cn(
+              "w-full bg-white rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800 pt-4 peer placeholder-shown:pt-0 focus:border-gray-800 transition-all outline-none",
+              error && "!border-red-900"
+            )
+          }
+        />
 
-      <label
-        htmlFor={inputId}
-        className="absolute text-xs left-[13px] top-2 pointer-events-none text-gray-700 peer-placeholder-shown:text-base peer-placeholder-shown:top-3.5 transition-all"
-      >
-        {placeholder}
-      </label>
-    </div>
-  )
-}
+        <label
+          htmlFor={inputId}
+          className="absolute text-xs left-[13px] top-2 pointer-events-none text-gray-700 peer-placeholder-shown:text-base peer-placeholder-shown:top-3.5 transition-all"
+        >
+          {placeholder}
+        </label>
+
+        {error && (
+          <div className="flex gap-2 items-center mt-2 text-red-900 ">
+            <CrossCircledIcon />
+            <span className="text-xs">{error}</span>
+          </div>
+        )}
+      </div>
+    )
+  }
+)
