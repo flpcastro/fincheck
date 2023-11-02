7a81904528ce47ff374dd0f9c4f3de7093af6fae
Felipe <flpc.dev@gmail.com>
2023-11-02 04:38:37 -0300

    feat: config usequery and create signin and signout implementations

--- frontend/src/app/contexts/AuthContext.tsx
+++ frontend/src/app/contexts/AuthContext.tsx
@@ -1,7 +1,14 @@
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useCallback, useEffect, useState } from "react";
+import { localStorageKeys } from "../config/localStorageKeys";
+import { useQuery } from "@tanstack/react-query";
+import { usersService } from "../services/usersService";
+import toast from "react-hot-toast";
+import { LaunchScreen } from "../../view/components/LaunchScreen";
 
 interface AuthContextProps {
   signedIn: boolean;
+  signIn(accessToken: string): void;
+  signOut(): void;
 }
 
 interface AuthProviderProps {
@@ -11,11 +18,44 @@ interface AuthProviderProps {
 export const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
 
 export function AuthProvider({ children }: AuthProviderProps) {
+  const [signedIn, setSignedIn] = useState<boolean>(() => {
+    const storedAccessToken = localStorage.getItem(localStorageKeys.ACCESS_TOKEN);
+
+    return !!storedAccessToken;
+  });
+
+  const { isError, isSuccess, isFetching } = useQuery({
+    queryKey: ['loggedUser'],
+    queryFn: async () => usersService.Me(),
+    enabled: signedIn,
+    staleTime: Infinity,
+  });
+
+  const signIn = useCallback((accessToken: string) => {
+    localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
+    setSignedIn(true);
+  }, [])
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
+    setSignedIn(false);
+  }, [])
+
+  useEffect(() => {
+    if (isError) {
+      toast.error('Sua sessão expirou!')
+      signOut();
+    }
+  }, [isError, signOut])
+
   return (
     <AuthContext.Provider value={{
-      signedIn: false
+      signedIn: isSuccess && signedIn,
+      signIn,
+      signOut
     }}>
-      {children}
+      <LaunchScreen isLoading={isFetching} />
+      {!isFetching && children}
     </AuthContext.Provider>
   )
 }
